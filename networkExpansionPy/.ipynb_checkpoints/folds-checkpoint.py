7e671f663d7feb6d3f000d647787845529cafc69
jgoldford <goldford.joshua@gmail.com>
2021-02-08 17:38:55 -0500

    added fold expansion module and scc code

---
+++ networkExpansionPy/.ipynb_checkpoints/folds-checkpoint.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pandas as pd
+import os
+from copy import copy, deepcopy
+
+# define asset path
+asset_path,filename = os.path.split(os.path.abspath(__file__))
+asset_path = asset_path + '/assets'
+
+
+def rule2rn(foldSet,x):
+    if x.issubset(foldSet):
+    #if foldSet.issubset(x)
+        return True
+    else:
+        return False
+    
+def folds2rn(rules_sub,foldSet):
+    feasible = rules_sub['fold_sets'].apply(lambda x: rule2rn(foldSet,x))
+    rns = rules_sub[feasible].rn.unique().tolist()
+    return rns
+
+
+class FoldRules:
+    
+    def __init__(self):
+        # load the data
+        self.rules = None;
+        self.rn = None
+        self.folds = None;
+        
+    def copy(self):
+        return deepcopy(self)
+        
+    def setRules(self,path = '/ecode/ecod2rn.ec3.07Feb2021.csv'):
+        rules = pd.read_csv(asset_path + path)
+        self.rns = rules.rn.unique().tolist()
+        folds =  rules['rule'].apply(lambda x: set(x.split('_')))
+        folds = [item for sublist in folds for item in sublist]
+        self.folds = list(set(folds))
+        rules['fold_sets']= rules['rule'].apply(lambda x: set(x.split('_')))
+        self.rules = rules
+        
+    def folds2reactions(self,foldSet):
+        # make sure foldSet is of "set" type
+        if type(foldSet) != set:
+            if type(foldSet) != list:
+                foldSet = set([foldSet])
+            else:
+                foldSet = set(foldSet)
+                
+        rns = folds2rn(self.rules,foldSet)
+        return rns
+    
+    def removeFolds(self,folds_remove):
+        if type(folds_remove) != set:
+            if type(folds_remove) != list:
+                folds_remove = set([folds_remove])
+            else:
+                folds_remove = set(folds_remove)
+                
+        rulesRemoval = self.rules.fold_sets.apply(lambda x: folds_remove.issubset(x)) 
+        self.rules = self.rules[~rulesRemoval]
+        folds_remove_list = list(folds_remove)
+        self.folds = [x for x in self.folds if x not in folds_remove_list]
+    
+# define a function to run network expansion using a fold set
+# depends on metabolism object and foldRules objects
+def fold_expansion(metabolism,foldRules,fold_set,cpd_set,rxns_seed):
+    rxns_feasible = foldRules.folds2reactions(fold_set)
+    rxns_total = list(rxns_feasible) + list(rxns_seed)
+    m = metabolism.copy()
+    m.subnetwork(rxns_total)
+    cpds_ne,rxns_ne = m.expand(list(cpd_set))
+    return cpds_ne,rxns_ne,rxns_feasible
