44a51c29e7c916005d85c04fcb2abbe4e06166e5
hbsmith <hbs@asu.edu>
2021-06-01 16:43:45 +0900

    added outline of tests

---
+++ test/test_lib.py
@@ -0,0 +1,16 @@
+import unittest
+import networkExpansionPy.lib as ne
+
+class TestGlobalMetabolicNetworkInit(unittest.TestCase):
+
+    def test_kegg_og_no_args(self):
+        kegg_og = ne.GlobalMetabolicNetwork()
+
+    def test_kegg_og(self):
+        kegg_og = ne.GlobalMetabolicNetwork("KEGG_OG")
+
+    def test_ecg(self):
+        ecg = ne.GlobalMetabolicNetwork("ecg")
+
+    def test_KEGG(self):
+        kegg = ne.GlobalMetabolicNetwork("KEGG")
